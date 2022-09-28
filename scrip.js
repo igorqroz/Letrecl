4de3cd275f4ae79b151fb2b795051ab0f4749261
Igor Queiroz <igorqueirozdossantoslima_@hotmail.com>
2022-09-28 18:18:51 -0300

    Letreco

--- scrip.js
+++ scrip.js
@@ -1,24 +1,68 @@
-let palavraDoDia = "navio"
+const palavraDoDia = "navio"
 
-function checarLetra(letraDigitada) {
-    let existeLetra = palavraDoDia.indexOf(letraDigitada);
-    if (existeLetra != -1) {
-        console.log('Verde')
-    } else {
-        console.log('Vermelho')
+function execute() {
+    tentaviva1()
+    tentaviva2()
+    tentaviva3()
+    tentaviva4()
+    tentaviva5()
+}
+
+function tentaviva1() {
+    let element = document.getElementById('tentativa1').children;
+    for (let i = 0; i < element.length; i++) {
+        let child = element[i];
+        child.style.backgroundColor = checarLetra(child.value, i);
+        console.log(`Letra: ${child.value}, Posição: ${i}`)
+        child.style.color = "black";
     }
 }
-function execute() {
-    let element = document.getElementById('tentativa');
-    let children = element.children;
-    let palaravra = [];
-    let existeLetra = palavraDoDia.indexOf()
-    for (let i = 0; i < children.length; i++) {
-        let child = children[i];
-        palaravra.push(child.value);
-        console.log(palaravra)
-    }
-    // console.log(palaravra.join(""));
-    checarLetra(palaravra.join(""))
 
+function tentaviva2() {
+    let element = document.getElementById('tentativa2').children;
+    for (let i = 0; i < element.length; i++) {
+        let child = element[i];
+        child.style.backgroundColor = checarLetra(child.value, i);
+        console.log(`Letra: ${child.value}, Posição: ${i}`)
+        child.style.color = "black";
+    }
+}
+function tentaviva3() {
+    let element = document.getElementById('tentativa3').children;
+    for (let i = 0; i < element.length; i++) {
+        let child = element[i];
+        child.style.backgroundColor = checarLetra(child.value, i);
+        console.log(`Letra: ${child.value}, Posição: ${i}`)
+        child.style.color = "black";
+    }
+}
+function tentaviva4() {
+    let element = document.getElementById('tentativa4').children;
+    for (let i = 0; i < element.length; i++) {
+        let child = element[i];
+        child.style.backgroundColor = checarLetra(child.value, i);
+        console.log(`Letra: ${child.value}, Posição: ${i}`)
+        child.style.color = "black";
+    }
+}
+function tentaviva5() {
+    let element = document.getElementById('tentativa5').children;
+    for (let i = 0; i < element.length; i++) {
+        let child = element[i];
+        child.style.backgroundColor = checarLetra(child.value, i);
+        console.log(`Letra: ${child.value}, Posição: ${i}`)
+        child.style.color = "black";
+    }
+}
+
+function checarLetra(letraDigitada, indice) {
+
+    let exiteLetra = palavraDoDia.toUpperCase().indexOf(letraDigitada.toUpperCase());
+    if (exiteLetra != -1 && exiteLetra == indice) {
+        return "green"
+    } else if (exiteLetra != -1) {
+        return "yellow"
+    } else {
+        return "red"
+    }
 }
